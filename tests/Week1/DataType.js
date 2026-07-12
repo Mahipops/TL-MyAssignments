23c1b8b4103ffe01f3c46001edf2ac99186e21b5
Mahaalakshimi VBa <mahavba@gmail.com>
2026-07-12 11:27:59 +0530

    New playwright

---
+++ tests/Week1/DataType.js
@@ -0,0 +1,15 @@
+var  firstName ="Javascript";
+var companyName="Apple" ;
+var mobileNumber ="123456899" ;
+var isAutomation=true  ;   
+var hasPlaywright ; 
+console.log(firstName);
+console.log(typeof firstName);
+console.log(companyName);
+console.log(typeof companyName);
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+console.log(isAutomation);
+console.log(typeof isAutomation);
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
---
+++ tests/Week1/Scoping.js
@@ -0,0 +1,18 @@
+let genderType ="Female"
+function printGender()
+{
+    let varColor="brown";
+    if(genderType.startsWith("Female"))
+    {
+        var age=30;
+        let varColor="pink"
+        console.log(varColor)
+    }
+    console.log(age)
+    console.log(varColor)
+}
+printGender()
+console.log(genderType)
+genderType="Male"
+printGender()
+console.log(genderType)
